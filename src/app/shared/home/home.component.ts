56574b9e6d70ac89fd46ec4a32dffc5729dbd0af
Ali Ahsan <aliahsan8751@gmail.com>
2026-03-30 09:54:47 +0500

    added new game and home component

--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [RouterLink]
+  imports: [RouterModule]
 })
 export class HomeComponent {
 
